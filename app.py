e557d642a143656268650d76a89b4f4995d92b7e
Tazi Mohannad <77018169+mohannadtazi@users.noreply.github.com>
2024-10-09 23:57:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,19 @@ from PIL import Image
 import tempfile
 import os
 from src_py.crew import crew
+import asyncio
 
+def get_or_create_eventloop():
+    try:
+        return asyncio.get_event_loop()
+    except RuntimeError as ex:
+        if "There is no current event loop in thread" in str(ex):
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            return asyncio.get_event_loop()
+
+loop = asyncio.new_event_loop()
+asyncio.set_event_loop(loop)
 # Set favicon and page configuration
 fav_icon = Image.open("CareerCraft.png")
 st.set_page_config(page_title="Career Craft", page_icon=fav_icon)
