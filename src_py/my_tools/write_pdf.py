488f9e7fa3e4dc67cc5739b3d8666cbcc081f9ff
Tazi Mohannad <77018169+mohannadtazi@users.noreply.github.com>
2024-10-09 23:23:28 +0100

    Update write_pdf.py

--- src_py/my_tools/write_pdf.py
+++ src_py/my_tools/write_pdf.py
@@ -1,16 +1,16 @@
-import fitz 
-from crewai_tools import BaseTool
+#import fitz 
+#from crewai_tools import BaseTool
 
 
-class FileWriteTool(BaseTool):
-    name: str = "PDF Writer"
-    description: str = "Write the content passed to it in a PDF file and returns it."
+#class FileWriteTool(BaseTool):
+#    name: str = "PDF Writer"
+#    description: str = "Write the content passed to it in a PDF file and returns it."
 
-    def _run(self,content: str) -> str:
+#    def _run(self,content: str) -> str:
     # Create a canvas object
-      pdf_document = fitz.open()
-      page = pdf_document.new_page()
-      page.insert_text((72, 72), content, fontsize=12)
+#      pdf_document = fitz.open()
+#      page = pdf_document.new_page()
+#      page.insert_text((72, 72), content, fontsize=12)
 
-      pdf_document.save("custom_cv.pdf")
-      pdf_document.close()
+#      pdf_document.save("custom_cv.pdf")
+#      pdf_document.close()
