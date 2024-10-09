2b363ecc738e73ffd4a9a3212a039db11bdba62f
Tazi Mohannad <77018169+mohannadtazi@users.noreply.github.com>
2024-10-09 23:24:01 +0100

    Update tools.py

--- src_py/tools.py
+++ src_py/tools.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 load_dotenv()
 from src_py.my_tools.read_pdf import FileReadTool
 #from my_tools.send_draft import email_tool
-from src_py.my_tools.write_pdf  import FileWriteTool
+#from src_py.my_tools.write_pdf  import FileWriteTool
 import os
 from crewai_tools import SerperDevTool, SeleniumScrapingTool, CodeInterpreterTool
 
@@ -16,7 +16,7 @@ rag_search_tool= SeleniumScrapingTool()
 #file_read_tool = FileReadTool(file_path='src_py\Data\PFA-mohannad_tazi--DataScience__BigData.pdf')
 file_read_tool = FileReadTool()
 codding_tool = CodeInterpreterTool()
-file_writer_tool = FileWriteTool()
+#file_writer_tool = FileWriteTool()
 
 
 
